dd9b6a19e93832f58c92ac1cd2e371a577ea258d
Nick Chuykov <che4151999@gmail.com>
2020-03-01 20:40:36 +0200

    01.03

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-#https://t.me/nickchuykov
-from flask import Flask, escape, request
+#telegram @nickchuykov
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello():
-  #  name = request.args.get("name", "World")
-   # return f'Chuykov Nikita TI-82'
-#@app.route('/first')
-#def variable_1():
-   # a,b,c = "kpi",None,1488
-   
-   # return f'{a},{b},{c}'
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Chuykov Nick TI-82'
 
-@app.route('/second', methods=['GET', 'POST'])
+@app.route('/first')
+def first():
+    a = None
+    b = 24
+    c = 'Python is Cool!'
+    return f'{a}, {b}, {c}'
+    
+@app.route('/second',methods = ["GET", "POST"])
 def second():
-    import pdb;
-    pdb.set_trace()
-    if request.method == 'GET':
-        return render_template("Front.html")
+    if  request.method == 'GET':
+        return render_template('second.html')
     if request.method == 'POST':
-        text = request.form['text']
-        reversed_string = string[::-1]
-        
-	
+        text = request.form.get('text')
+        first = "".join(reversed(text))
+        second = "".join([x for x in text[::-1]])
+        third = text[::-1]
+        return f'reversed: \t{first}<br/>comprehention: \t{second}<br/>slicing: \t{third}'
 
 
-
+@app.route('/third',methods = ["GET", "POST"])
+def third():
+    pass
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
