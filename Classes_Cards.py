c2c29c8b89ed8b9dd017b285938d6ee5ded6db39
Nick Chuykov <che4151999@gmail.com>
2020-03-03 21:34:32 +0200

    third com

--- Classes_Cards.py
+++ Classes_Cards.py
@@ -1,5 +1,5 @@
-import itertools as it
-import random as r
+import random
+import itertools
 
 
 class Card:
@@ -17,20 +17,19 @@ class Deck:
     def __init__(self):
         self.deck = []
         suits = ('\u2660', '\u2663', '\u2665', '\u2666')
-        ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack',
-                 'Queen', 'King', 'Ace', 'Joker')
-        for suit, rank in it.product(suits, ranks):
+        ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+        for suit, rank in itertools.product(suits, ranks):
             self.deck.append(Card(suit, rank))
 
     def __str__(self):  # prints whole deck
         return 'The deck Is: ' + '\n'.join([str(card) for card in self.deck])
 
     def shuffle(self):  # shuffles the deck
-        r.shuffle(self.deck)
+        random.shuffle(self.deck)
 
     def get_random(self):  # returns random card from deck
         try:
-            return str(r.choice(self.deck))
+            return str(random.choice(self.deck))
         except IndexError:
             self.__init__()
             return "No more cards in deck, It was re-created.\nNow you can get random card."
@@ -52,4 +51,3 @@ class Deck:
             return "Your index is out of range."
         except ValueError:
             return "Index should be a number."
-
